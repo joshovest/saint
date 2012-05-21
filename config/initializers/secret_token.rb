a4d64e68e81fd734214d16b26afd2fce3d4b0b49
Josh West <joshovest@gmail.com>
2012-05-21 15:32:12 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saint::Application.config.secret_token = '183f887354222b7895221b5987d42ef7be99067e99205158539da56922230e7d702778c736976c1f0e8dcc59deb5d7d04d68e07fa2907e3708c39274ab2d8478'
