6e42c32826c9c322c4d29694db69af52652f24c7
Josh West <joshovest@gmail.com>
2012-05-23 16:30:38 -0600

    User model + secure passwords

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Saint::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  get "users/new"
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   resources :cloud_matches
 
@@ -62,7 +64,7 @@ Saint::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
