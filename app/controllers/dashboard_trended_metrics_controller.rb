f1dd12f2c7c0f0e74f401b5c8ebf371048fdd731
Josh West <joshovest@gmail.com>
2012-10-12 14:50:00 -0600

    automated data retrieval (2)

--- app/controllers/dashboard_trended_metrics_controller.rb
+++ app/controllers/dashboard_trended_metrics_controller.rb
@@ -38,7 +38,6 @@ class DashboardTrendedMetricsController < ApplicationController
     
     o = Omni.new
     if Rails.env.production?
-      flash[:success] = "Data has been re-requested."
       o.delay.load_trended_metrics(suite_id, metrics, segment, t_start, t_end, granularity)
     else
       o.load_trended_metrics(suite_id, metrics, segment, t_start, t_end, granularity)
